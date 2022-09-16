fefcba42adc9a7f7fdb8122e7e7ffe0bed32a882
laporte727 <ecole.la.porte@gmail.com>
2022-09-16 09:55:16 +0900

    textAlign.center

--- lib/domain/format.dart
+++ lib/domain/format.dart
@@ -13,25 +13,26 @@ class Format extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      textAlign: TextAlign.center,
       decoration: InputDecoration(
         contentPadding: const EdgeInsets.all(5.0),
         hintText: hintText,
         hintStyle: const TextStyle(
-            fontSize: 12,
+            fontSize: 14,
             color: Colors.green),
-        fillColor: Colors.lime,
+        fillColor: Colors.lightGreen[100],
         filled: true,
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
           borderSide: const BorderSide(
-            color: Colors.lime,
+            color: Colors.lightGreen,
             width: 2.0,
           ),
         ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(16),
           borderSide: const BorderSide(
-            color: Colors.lime,
+            color: Colors.lightGreen,
             width: 1.0,
           ),
         ),
